02007371b791847ef351015d5c9677be550c0c6e
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2019-09-19 14:28:41 -0400

    Disable default security headers

---
+++ src/main/java/example/bank/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package example.bank.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(requests ->
+                        requests.anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults())
+                .headers(AbstractHttpConfigurer::disable);
+    }
+}
